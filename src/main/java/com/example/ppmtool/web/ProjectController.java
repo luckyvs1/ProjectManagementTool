6efde9f94d6e92f212e00cdccfb1072a165868af
lvs1 <lvs1@sfu.ca>
2019-09-20 20:45:49 -0700

    Refactor the project controller by making a error validation service for reuse

--- src/main/java/com/example/ppmtool/web/ProjectController.java
+++ src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.example.ppmtool.web;
 
 import com.example.ppmtool.domain.Project;
+import com.example.ppmtool.services.MapValidationErrorService;
 import com.example.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,19 +25,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if (result.hasErrors()){
-
-            Map<String, String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null){
+            return errorMap;
         }
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
